54f1b40ace7d7d9cf9917b3fe8126a7f5b88c665
ekbhard <ekbhard@gmail.com>
2020-06-28 20:41:28 +0500

    Initial import

---
+++ jenkinsfile/run.groovy
@@ -0,0 +1,68 @@
+
+pipeline {
+    agent{node('master')}
+    stages {
+        stage('Clean workspace & dowload dist') {
+            steps {
+                script {
+                    cleanWs()
+                    withCredentials([
+                        usernamePassword(credentialsId: 'srv_sudo',
+                        usernameVariable: 'username',
+                        passwordVariable: 'password')
+                    ]) {
+                        try {
+                            sh "echo '${password}' | sudo -S docker stop isng"
+                            sh "echo '${password}' | sudo -S docker container rm isng"
+                        } catch (Exception e) {
+                            print 'container not exist, skip clean'
+                        }
+                    }
+                }
+                script {
+                    echo 'Update from repository'
+                    checkout([$class                           : 'GitSCM',
+                              branches                         : [[name: '*/master']],
+                              doGenerateSubmoduleConfigurations: false,
+                              extensions                       : [[$class           : 'RelativeTargetDirectory',
+                                                                   relativeTargetDir: 'auto']],
+                              submoduleCfg                     : [],
+                              userRemoteConfigs                : [[credentialsId: 'IvanSitnikovGit', url: 'https://github.com/sitozzz/jenkins_education.git']]])
+                }
+            }
+        }
+        stage ('Build & run docker image'){
+            steps{
+                script{
+                     withCredentials([
+                        usernamePassword(credentialsId: 'srv_sudo',
+                        usernameVariable: 'username',
+                        passwordVariable: 'password')
+                    ]) {
+
+                        sh "echo '${password}' | sudo -S docker build ${WORKSPACE}/auto -t ivan_sitnikov_nginx"
+                        sh "echo '${password}' | sudo -S docker run -d -p 8123:80 --name isng -v /home/adminci/is_mount_dir:/stat ivan_sitnikov_nginx"
+                    }
+                }
+            }
+        }
+        stage ('Get stats & write to file'){
+            steps{
+                script{
+                    withCredentials([
+                        usernamePassword(credentialsId: 'srv_sudo',
+                        usernameVariable: 'username',
+                        passwordVariable: 'password')
+                    ]) {
+                        
+                        sh "echo '${password}' | sudo -S docker exec -t isng bash -c 'df -h > /stat/stats.txt'"
+                        sh "echo '${password}' | sudo -S docker exec -t isng bash -c 'top -n 1 -b >> /stat/stats.txt'"
+                    }
+                }
+            }
+        }
+        
+    }
+
+    
+}
